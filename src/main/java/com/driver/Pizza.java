5e0254dbe4005b5bfbdd4035247137cfdc918208
Pushpak <pushpakmali44@gmail.com>
2023-05-24 00:48:01 +0530

    All changes done

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,29 +6,78 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    public static int vegPizzaBasePrice = 300;
+    public static int nonVegPizzaBasePrice = 400;
+    public static int cheesePrice = 80;
+    public static int toppingPriceForVeg = 70;
+    public static int toppingPriceForNonVeg = 120;
+    private boolean isCheeseAdded = false;
+    private boolean isToppingAdded = false;
+    private boolean isTakeAwayAdded = false;
+    public static int takeAwayPrice = 20;
+    private int myToppingPrice = 0;
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        this.price = isVeg ? vegPizzaBasePrice : nonVegPizzaBasePrice;
+        this.bill = "Base Price Of The Pizza: "+this.price+"\n";
+        this.isCheeseAdded = false;
+        this.isToppingAdded = false;
+        this.isTakeAwayAdded = false;
     }
 
     public int getPrice(){
+        if(isCheeseAdded){
+            this.price += cheesePrice;
+        }
+
+        if(isToppingAdded){
+            this.price += this.myToppingPrice;
+        }
+
+        if(isTakeAwayAdded){
+            this.price += takeAwayPrice;
+        }
+
         return this.price;
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if(isCheeseAdded == false){
+            isCheeseAdded = true;
+        }
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(isToppingAdded == false){
+            isToppingAdded = true;
+            this.myToppingPrice = isVeg ? toppingPriceForVeg : toppingPriceForNonVeg;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if(isTakeAwayAdded == false){
+            isTakeAwayAdded = true;
+        }
     }
 
     public String getBill(){
-        // your code goes here
-        return this.bill;
+        String bill = this.bill;
+
+        if(isCheeseAdded){
+            bill += "Extra Cheese Added: "+this.cheesePrice+"\n";
+        }
+
+        if(isToppingAdded){
+            bill += "Extra Toppings Added: "+this.myToppingPrice+"\n";
+        }
+
+        if(isTakeAwayAdded){
+            bill += "Paperbag Added: "+takeAwayPrice+"\n";
+        }
+
+        bill += "Total Price: "+this.price+"\n";
+
+        return bill;
     }
 }
